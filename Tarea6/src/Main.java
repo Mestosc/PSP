b80718d9b19507487993d1c46d7b1f05922a27d2
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-09-24 09:31:10 +0200

    El mostrar la informacion por pantalla paso a hacerlo por un metodo ya que es muy repetitivo

--- Tarea6/src/Main.java
+++ Tarea6/src/Main.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 //import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
@@ -12,7 +13,7 @@ public class Main {
         String ok = "\033[32m[OK] \033[37m";
         ArrayList<String> proceso = new ArrayList<>(List.of("ping","4",sitio));
         if (System.getProperty("os.name").toLowerCase().startsWith("windows")) {
-            proceso.add(1,"-n");
+            proceso.add(1,"/n");
         } else if (System.getProperty("os.name").toLowerCase().startsWith("linux")) {
             proceso.add(1,"-c");
         }
@@ -21,30 +22,26 @@ public class Main {
         //pb.redirectError(new File("ping_error.log")); // Redireccion de errores a fichero
         try {
             Process p = pb.start();
-            try (BufferedReader buf = new BufferedReader(new InputStreamReader(p.getErrorStream(), StandardCharsets.UTF_8))) {
-                String linea;
-                while ((linea=buf.readLine())!=null) {
-                    String lineaF = "";
-                    lineaF += error;
-                    lineaF += linea;
-                    System.out.println(lineaF);
-                }
-            }
-            try (BufferedReader buf = new BufferedReader(new InputStreamReader(p.getInputStream(),StandardCharsets.UTF_8))) {
-                String linea;
-                while ((linea=buf.readLine())!=null) {
-                    String lineaF = "";
-                    lineaF += ok;
-                    lineaF += linea;
-                    System.out.println(lineaF);
-                }
-            }
+            mostrarInformacionRequerimientos(p.getErrorStream(), error);
+            mostrarInformacionRequerimientos(p.getInputStream(), ok);
             int estado = p.waitFor();
             System.out.println("Operacion completada. Codigo de salida " + estado);
         } catch (IOException e) {
             System.out.println("Problema de entrada salida " + e);
         } catch (InterruptedException e) {
             System.out.println("Interrupcion " + e);
+        }
+    }
+
+    private static void mostrarInformacionRequerimientos(InputStream p, String ok) throws IOException {
+        try (BufferedReader buf = new BufferedReader(new InputStreamReader(p, StandardCharsets.UTF_8))) {
+            String linea;
+            while ((linea = buf.readLine()) != null) {
+                String lineaF = "";
+                lineaF += ok;
+                lineaF += linea;
+                System.out.println(lineaF);
+            }
         }
     }
 }
