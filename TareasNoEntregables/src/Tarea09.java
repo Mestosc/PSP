1d10915fb1940117761fc068c76ce514e988d847
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-09-30 09:11:05 +0200

    Acabando Tarea09

---
+++ TareasNoEntregables/src/Tarea09.java
@@ -0,0 +1,21 @@
+public class Tarea09 extends Thread {
+    int limite;
+    public Tarea09(int n) {
+        this.limite = n;
+    }
+    private void mostrarCalculoFibonacci(int num1, int num2) {
+        int result = num1 + num2;
+        System.out.println(num1);
+        if (result <= limite) {
+            mostrarCalculoFibonacci(num2, result);
+        }
+    }
+    @Override
+    public void run() {
+        mostrarCalculoFibonacci(1,1);
+    }
+    public static void main(String[] args) {
+        Tarea09 t1 = new Tarea09(100);
+        t1.start();
+    }
+}
