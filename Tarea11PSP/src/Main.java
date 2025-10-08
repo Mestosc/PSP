d3a07ee87271d2527df36fd52c7f03cd0235d101
Oscar <oscarodriguez56@gmail.com>
2025-10-08 08:51:37 +0200

    Mejorando el que el hilo principal sea vigilado

--- Tarea11PSP/src/Main.java
+++ Tarea11PSP/src/Main.java
@@ -2,7 +2,12 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        long inicio = System.currentTimeMillis();
         EfectoDomino hilo = new EfectoDomino(6);
         hilo.start();
+        EfectoDomino.vigilanteDeHilo(hilo);
+        long fin = System.currentTimeMillis();
+        System.out.println("Acabó hilo " + hilo.getName() + " Tiempo total: " + (fin - inicio) + " ms");
+
     }
 }
