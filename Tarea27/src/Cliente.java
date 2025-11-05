f065501f56f8be82fa232434d8ac701853e7eaf8
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-11-05 09:16:44 +0100

    Tarea 27 finalizada

---
+++ Tarea27/src/Cliente.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Cliente {
+    public static void main(String[] args) {
+        InetSocketAddress dir = new InetSocketAddress("localhost",8000);
+        String linea;
+        Scanner scanner = new Scanner(System.in);
+        try (Socket socket = new Socket()) {
+            socket.connect(dir);
+            PrintWriter writer = new PrintWriter(socket.getOutputStream(),true);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            while (true) {
+                linea = scanner.nextLine();
+                if (linea.equalsIgnoreCase("adios")) {
+                    break;
+                }
+                writer.println(linea);
+                linea = reader.readLine();
+                System.out.println(linea);
+            }
+            scanner.close();
+        } catch (IOException e) {
+            System.out.println("Problemas de entrada salida");
+        }
+    }
+}
---
+++ Tarea27/src/Servidor.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Servidor {
+    public static void main(String[] args) {
+        InetSocketAddress dir = new InetSocketAddress("localhost",8000);
+        try (ServerSocket server = new ServerSocket()) {
+            server.bind(dir);
+            Socket socket = server.accept();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter writer = new PrintWriter(socket.getOutputStream(),true);
+            while (true) {
+                String linea = reader.readLine();
+                if (linea == null || linea.equalsIgnoreCase("adios")) {
+                    break;
+                }
+                writer.println("ECO: " + linea);
+            }
+        } catch (IOException e) {
+            System.out.println("Problemas");
+        }
+    }
+}
