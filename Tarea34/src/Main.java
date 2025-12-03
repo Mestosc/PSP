2bb5c457b54478da49b3e6889b86c0be6620bd62
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-12-03 10:06:54 +0100

    Añadidas peticiones de URL al usuario.

--- Tarea34/src/Main.java
+++ Tarea34/src/Main.java
@@ -9,7 +9,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Introduzca la primera URL: ");
         String url1 = sc.next();
+        System.out.print("Introduzca la segunda URL: ");
         String url2 = sc.next();
         HttpClient httpClient = HttpClient.newHttpClient();
         HttpRequest request1 = HttpRequest.newBuilder()
