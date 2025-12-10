f2203c293375adf0652935a93803e30326be5e5d
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-12-10 14:50:12 +0100

    Arreglando condicion incorrecta

--- Tarea36/src/main/java/org/example/LectorCorreo.java
+++ Tarea36/src/main/java/org/example/LectorCorreo.java
@@ -4,10 +4,16 @@ import jakarta.mail.*;
 
 import java.util.Optional;
 
+/**
+ * Lector de Correo Electronico, se encarga de leer los correos electronicos, accediendo a la bandeja que tu le digas
+ * @see java.lang.AutoCloseable
+ * @version 1.0
+ * @author Oscar
+ */
 public class LectorCorreo implements AutoCloseable {
     public final static String IMAP = "imap";
     public final static String POP_3 = "pop3";
-    private Store store;
+    private final Store store;
     public LectorCorreo(Session session,String storeName) {
         try {
             this.store = session.getStore(storeName);
@@ -22,6 +28,13 @@ public class LectorCorreo implements AutoCloseable {
     public LectorCorreo(Session session) {
         this(session,IMAP);
     }
+
+    /**
+     * Se encarga de realizar la conexion con el servidor de correo
+     * @param userName el nombre de usuario con el que realizas la conexion
+     * @param password la contraseña con la que realizas la conexion
+     * @return Verdadero si la conexion se realizo sino falso
+     */
     public boolean connect(String userName, String password) {
         try {
             System.out.println("Conectando con usuario: " + userName);
@@ -33,6 +46,12 @@ public class LectorCorreo implements AutoCloseable {
             return false;
         }
     }
+
+    /**
+     * Lee los mensajes
+     * @param folder la carpeta de la que se leen los mensajes
+     * @return un Optional con los mensajes, si ha habido un fallo al leerlos lo devolvera vacio
+     */
     public Optional<Message[]> leerCorreosRecientes(String folder) {
         try (Folder folder1 = store.getFolder(folder)) {
             folder1.open(Folder.READ_ONLY);
@@ -45,7 +64,7 @@ public class LectorCorreo implements AutoCloseable {
 
     @Override
     public void close() throws Exception {
-        if (store.isConnected()||store!=null) {
+        if (store!=null && store.isConnected()) {
             try {
                 store.close();
             } catch (MessagingException e) {
